84d821f69561df0056fd69bea563584498281b29
yongwoon <yonyon.japan@gmail.com>
2025-01-28 20:34:05 +0900

    Refactoring: pdf-based-qa.py

--- pages/pdf-based-qa.py
+++ pages/pdf-based-qa.py
@@ -1,17 +1,16 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
-from langchain import hub
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.messages.chat import ChatMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, load_prompt, PromptTemplate
+from langchain_core.prompts import load_prompt
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from utils.prompt_loader import PromptLoader
+from utils.langsmith_config import setup_langsmith
 
 load_dotenv()
 st.title("pdf based QA")
@@ -33,10 +32,19 @@ if "chain" not in st.session_state:
     # file upload 전에 chain 을 생성하지 않음을 의마
     st.session_state["chain"] = None
 
+    # Setup LangSmith
+    setup_langsmith(
+        project_name="trace-PDF",
+        enable_tracing=True
+    )
+
 with st.sidebar:
     clear_btn = st.button("Reset Chat")
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-    selected_prompt = "prompts/pdf/en/pdf-rag.yaml"
+    selected_model = st.selectbox(
+        "Select gpt Model",
+        ["gpt-4o", "gpt-4-turbo", "gpt-4o-mini"],
+        index=0)
 
 def print_messages():
     for chat_message in st.session_state["messages"]:
@@ -90,29 +98,15 @@ def embed_file(file):
 # 그 후 사용자가 입력을 하면 create_chain() 함수가 호출됩니다
 # 따라서 create_chain() 함수는 실제로 사용되기 전에 정의되어 있어야 정상적으로 작동합니다.
 # 이는 Python의 기본적인 스코프 규칙을 따르는 것으로, 코드의 안정성과 가독성을 위해 함수는 사용되기 전에 정의되어야 합니다.
-def create_chain(retriever: VectorStoreRetriever):
-    # ========== RAG 처리 =============
-    # 단계 6: 프롬프트 생성(Create Prompt)
-    prompt = PromptTemplate.from_template(
-        """You are an assistant for question-answering tasks.
-    Use the following pieces of retrieved context to answer the question.
-    If you don't know the answer, just say that you don't know.
-    Answer in Korean.
+def create_chain(retriever: VectorStoreRetriever, model_name="gpt-4o"):
+    # ========== RAG process =============
+    # 単階 6: Create Prompt
+    prompt = load_prompt("prompts/pdf/en/pdf-rag.yaml")
 
-    #Context:
-    {context}
+    # 단계 7: LLM 생성
+    llm = ChatOpenAI(model_name=model_name, temperature=0)
 
-    #Question:
-    {question}
-
-    #Answer:"""
-    )
-
-    # 단계 7: 언어모델(LLM) 생성
-    # 모델(LLM) 을 생성합니다.
-    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
-
-    # 단계 8: 체인(Chain) 생성
+    # 단계 8: Chain 생성
     chain = (
         {"context": retriever, "question": RunnablePassthrough()}
         | prompt
@@ -124,9 +118,9 @@ def create_chain(retriever: VectorStoreRetriever):
 
 # ========== file uploader =============
 if uploaded_file:
-    # 파일 업로드 후, retriver 생성 (process time 이 오래 걸리는 작업)
+    # ファイル Upload 後, retriver 生成 (process time が 長く かかる 作業)
     retriever = embed_file(uploaded_file)
-    chain = create_chain(retriever)
+    chain = create_chain(retriever, model_name=selected_model)
     st.session_state["chain"] = chain
 
 # ========= Clear chat history =========
